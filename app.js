3a4a96491d12842e5e2a4d47c7a796f4eabeaf0e
nancycrojas <nanclarisaok@gmail.com>
2022-06-23 09:01:59 -0300

    Variables, datos y operadores - ejercicio 7

---
+++ app.js
@@ -0,0 +1,6 @@
+//Minutos a segundos
+
+//Crear un programa que pida ingresar una cantidad en minutos mediante un y muestre un mensaje con el resultado de la conversión en segundos.
+
+const minutos = prompt ("Por favor ingresa los minutos");
+alert("Conversión en segundos: " + minutos * 60 + " segundos");
